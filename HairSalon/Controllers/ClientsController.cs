80e30f69f340965fef7a114c14c9a89c9fb14220
ChrisRDavila <crossdavila@gmail.com>
2023-05-19 09:58:42 -0700

    use db in Clients controller

---
+++ HairSalon/Controllers/ClientsController.cs
@@ -0,0 +1,24 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc;
+using HairSalon.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace HairSalon.Controllers
+{
+  public class ClientsController : Controller
+  {
+    private readonly HairSalonContext _db;
+
+    public ClientsController(HairSalonContext db)
+    {
+      _db = db;
+    }
+
+    public ActionResult Index()
+    {
+      List<Client> model = _db.Items.ToList();
+      return View(model);
+    }
+  }
+}
