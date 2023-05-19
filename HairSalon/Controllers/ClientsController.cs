3c82b2097d4c9917e1a5f87312de80c4c44e5c42
ChrisRDavila <crossdavila@gmail.com>
2023-05-19 10:46:04 -0700

    add Client Details route in ClientCont and view page

--- HairSalon/Controllers/ClientsController.cs
+++ HairSalon/Controllers/ClientsController.cs
@@ -33,5 +33,11 @@ namespace HairSalon.Controllers
       _db.SaveChanges();
       return RedirectToAction("Index");
     }
+
+    public ActionResult Details(int id)
+    {
+      Client thisClient = _db.Clients.FirstOrDefault(client => client.ClientId == id);
+      return View(thisClient);
+    }
   }
 }
