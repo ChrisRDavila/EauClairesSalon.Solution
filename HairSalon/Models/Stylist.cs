f74e541820967b8d31f7444faa3d17e760967d85
ChrisRDavila <crossdavila@gmail.com>
2023-05-19 09:48:40 -0700

    import schema from MySQL

--- HairSalon/Models/Stylist.cs
+++ HairSalon/Models/Stylist.cs
@@ -2,8 +2,8 @@ namespace HairSalon.Models
 {
   public class Stylist
   {
-    public string StylistName { get; set; }
     public int StylistId { get; set; }
+    public string StylistName { get; set; }
     public string HireDate { get; set; }
     public List<Client> Clients { get; set; }
   }
