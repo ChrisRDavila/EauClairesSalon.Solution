f06d396146cd7a7a140421843c0ea48e44f2e4df
ChrisRDavila <crossdavila@gmail.com>
2023-05-19 10:21:47 -0700

    add create client route to clientscontroller

--- HairSalon/Controllers/ClientsController.cs
+++ HairSalon/Controllers/ClientsController.cs
@@ -20,5 +20,18 @@ namespace HairSalon.Controllers
       List<Client> model = _db.Items.ToList();
       return View(model);
     }
+
+    public ActionResult Create()
+    {
+      return View();
+    }
+
+    [HttpPost]
+    public ActionResult Create(Client client)
+    {
+      _db.Clients.Add(client);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
   }
 }
